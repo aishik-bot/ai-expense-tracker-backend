720dbb488a0365a2da1605d8ba4a08e55934e0aa
Aishik Sikdar <aishik@freeflow.zone>
2024-12-29 04:26:58 +0530

    removed assert from import

--- src/utils/firebase.mjs
+++ src/utils/firebase.mjs
@@ -1,5 +1,5 @@
-import admin from 'firebase-admin';
-import serviceAccount from '../../serviceAccountKey.json' assert { type: 'json' };
+import admin from "firebase-admin";
+import serviceAccount from "../../serviceAccountKey.json";
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
